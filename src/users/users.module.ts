19bd4db680729542331366e3b54f00dafce7c566
Florian Rossiaud <florian@rossiaud.net>
2020-09-03 12:40:26 +0200

    fix: remove date scalar and use the default one

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '../prisma/prisma.module';
-import { DateScalar } from './common/scalars/date.scalar';
 import { UsersResolver } from './users.resolver';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UsersResolver, DateScalar],
+  providers: [UsersResolver],
 })
 export class UsersModule {}
